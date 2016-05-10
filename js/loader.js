5e248354af97ff564f4a819121476e757ffcea34
MengMW <TracyW@tarus-MacBook-Air.local>
2016-05-09 20:48:40 -0400

    用loader.js统一三个页面的header

---
+++ js/loader.js
@@ -0,0 +1,10 @@
+/**
+ * Created by TracyW on 5/9/16.
+ */
+
+$(document).ready(function(){
+    $('.navTitle').html('QArt Code')
+    $('#navLink1').html('Home');
+    $('#navLink2').html('Tutorial');
+    $('#navLink3').html('Gallery');
+});
